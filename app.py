c559c8955ffeffb91026e1308824cd4a8d2d5ac5
PJ Perez <pvperez1@up.edu.ph>
2023-05-30 19:25:45 +0800

    uploaded all codes

---
+++ app.py
@@ -0,0 +1,73 @@
+from flask import Flask, render_template, request, redirect, url_for
+from data import *
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/animals/<pet_type>')
+def animals(pet_type):
+    pets_list = read_pets_by_pet_type(pet_type)
+    return render_template("animals.html", pet_type=pet_type, pets=pets_list)
+
+@app.route('/animals/<int:pet_id>')
+def pet(pet_id):
+    pet = read_pet_by_pet_id(pet_id)
+    return render_template("pet.html",pet=pet)
+
+
+@app.route('/register')
+def register():
+    return render_template('register.html')
+
+@app.route('/processed', methods=['post'])
+def processing():
+    pet_data = {
+        "pet_type": request.form['pet_type'],
+        "name": request.form['pet_name'],
+        "breed": request.form['pet_breed'],
+        "age": request.form['pet_age'],
+        "description": request.form['pet_desc'],
+        "url": request.form['pet_url']
+    }
+    insert_pet(pet_data)
+    return redirect(url_for('animals', pet_type=request.form['pet_type']))
+
+
+@app.route('/modify', methods=['post'])
+def modify():
+    # 1. identify whether user clicked edit or delete
+    if request.form["modify"] == "edit":
+        pet_id = request.form["pet_id"]
+        pet = read_pet_by_pet_id(pet_id)
+        return render_template('update.html', pet=pet)
+    # if edit, then do this:
+        # retrieve record using id
+        # update record with new data
+        # redirect user to pet profile page
+    # if delete, then do this
+    elif request.form["modify"] == "delete":
+        pass
+        # retrieve record using id
+        # delete the record
+        # redirect user to pet list by pet type
+    pass
+
+@app.route('/update', methods=['post'])
+def update():
+    pet_data = {
+        "pet_id" : request.form["pet_id"],
+        "pet_type": request.form['pet_type'],
+        "name": request.form['pet_name'],
+        "breed": request.form['pet_breed'],
+        "age": request.form['pet_age'],
+        "description": request.form['pet_desc'],
+        "url": request.form['pet_url']
+    }
+    update_pet(pet_data)
+    return redirect(url_for('pet',pet_id = request.form['pet_id']))
+
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ data.py
@@ -0,0 +1,51 @@
+import sqlite3
+
+db_path = "pa.db"
+
+# This function conencts to the DB and returns a conn and cur objects
+def connect_to_db(path):
+    conn = sqlite3.connect(path)
+    # Converting tuples to dictionaries
+    conn.row_factory = sqlite3.Row
+    return (conn, conn.cursor())
+
+# This function returns pets by pet_type
+def read_pets_by_pet_type(pet_type):
+    conn, cur = connect_to_db(db_path)
+    query = 'SELECT * FROM pets WHERE animal_type = ?'
+    value = pet_type
+    results = cur.execute(query,(value,)).fetchall()
+    conn.close()
+    return results
+
+# This function retrieves 1 pet by pet_id
+def read_pet_by_pet_id(pet_id):
+    conn, cur = connect_to_db(db_path)
+    query = 'SELECT * FROM pets WHERE id = ?'
+    value = pet_id
+    result = cur.execute(query,(value,)).fetchone()
+    conn.close()
+    return result
+
+# This function inserts 1 pet data
+def insert_pet(pet_data):
+    conn, cur = connect_to_db(db_path)
+    query = 'INSERT INTO pets (animal_type, name, age, breed, description, url) VALUES (?,?,?,?,?,?)'
+    values = (pet_data['pet_type'], pet_data['name'],
+              pet_data['age'], pet_data['breed'],
+              pet_data['description'], pet_data['url'])
+    cur.execute(query,values)
+    conn.commit()
+    conn.close()
+
+# This function updates a record
+def update_pet(pet_data):
+    conn, cur = connect_to_db(db_path)
+    query = "UPDATE pets SET (animal_type=?, name=?, age=?, breed=?, description=?, url=?) WHERE id=?"
+    values = (pet_data['pet_type'], pet_data['name'],
+              pet_data['age'], pet_data['breed'],
+              pet_data['description'], pet_data['url'],
+              pet_data['pet_id'])
+    cur.execute(query, values)
+    conn.commit()
+    conn.close()
